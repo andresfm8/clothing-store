f1c7efbff124ce6a72a53b963c23c7116b8f3d9a
Andres Munevar <afmunevarm@hotmail.com>
2021-04-06 17:27:34 -0400

    Created signin and signup page as well as signin and form components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import Header from './components/header/header.component';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path="/" component={ HomePage }/>
-        <Route eaxt path="/shop" component= { ShopPage }/>
+        <Route path="/shop" component= { ShopPage }/>
+        <Route path="/signin" component={ SignInAndSignUpPage }/>
       </Switch>
     </div>
   );
