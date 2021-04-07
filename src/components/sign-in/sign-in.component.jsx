6d77faaed16beffe9abd83c93d6b8af0d8da474e
Andres Munevar <afmunevarm@hotmail.com>
2021-04-07 17:38:51 -0400

    Updated handleSubmit in  sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -17,8 +17,16 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ email: '', password: '' })
   }
