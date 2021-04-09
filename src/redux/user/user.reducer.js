e932abf3e538a709bc52fc08bf6dba60aec8b6e9
Andres Munevar <afmunevarm@hotmail.com>
2021-04-09 00:04:00 -0400

    Now user is redirected to home if signed in. Updated user reducer types

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
   currentUser: null
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {//If state is undefined, then set to null
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
