6cfb52981d83076ae28bfaf71b20165524ac74c8
Andres Munevar <afmunevarm@hotmail.com>
2021-04-08 23:53:32 -0400

    Implement user reducer and actions in header and app.js

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -22,6 +23,10 @@ const Header = ({ currentUser }) => (
       }
     </div>
   </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
