8a4e232b31d6adf8d65aef2088c4dd365c21d97c
Andres Munevar <afmunevarm@hotmail.com>
2021-04-21 13:04:34 -0400

    Convertes scss from collection components to styled components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,22 @@
+import { withRouter } from 'react-router';
+
 import CollectionItem from '../collection-item/collection-item.component.jsx';
 
-import './collection-preview.styles.scss';
+import { CollectionPreviewContainer, PreviewContainer, TitleContainer } from './collection-preview.styles.jsx';
 
-const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title"> { title.toUpperCase() } </h1>
-    <div className="preview">
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
+  <CollectionPreviewContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}> 
+      { title.toUpperCase() } 
+    </TitleContainer>
+    <PreviewContainer>
   	  {items
         .filter((item, idx) => idx < 4)//Return items where index < 4
         .map( item => (
           <CollectionItem key={item.id} item={item}/>
       ))}
-    </div>
-  </div>
+    </PreviewContainer>
+  </CollectionPreviewContainer>
 )
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
