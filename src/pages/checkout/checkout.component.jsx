fc74ee31c8945a56f22ea2b019e90a0ea529cf8a
Andres Munevar <afmunevarm@hotmail.com>
2021-04-21 23:22:13 -0400

    Finished conversion of scss to styled components

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,42 +5,47 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
-
-import './checkout.styles.scss';
+import {
+  CheckoutHeaderContainer,
+  CheckoutPageContainer,
+  TotalContainer,
+  WarningContainer,
+  HeaderBlockContainer
+} from './checkout.styles';
 
 const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
+  <CheckoutPageContainer>
+    <CheckoutHeaderContainer>
+      <HeaderBlockContainer>
         <span>Product</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Description</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Quantity</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Price</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Remove</span>
-      </div>
-    </div>
+      </HeaderBlockContainer>
+    </CheckoutHeaderContainer>
     {cartItems.map(cartItem => 
       <CheckoutItem key={cartItem.id} cartItem={ cartItem }/>
     )}
-    <div className="total">
+    <TotalContainer>
       <span>TOTAL: ${total}</span>
-    </div>
-    <div className="test-warning">
+    </TotalContainer>
+    <WarningContainer>
       Please use the following test credit card for payments: 
       <br/>
       4242 4242 4242 4242 - Exp: 01/22 or any future date - CVV: 123 or any 3 digits
       {/* Verify this is a valid test credit card */}
-    </div>
+    </WarningContainer>
     <StripeCheckoutButton price={total}/>
-  </div>
+  </CheckoutPageContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
